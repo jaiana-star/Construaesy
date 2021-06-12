dbcbb2d75ae5bd072024c4dc905aab3a28ef1dd2
jaiana-star <85024466+jaiana-star@users.noreply.github.com>
2021-06-11 22:27:15 -0300

    Initial commit

---
+++ src/main/java/devcalc/Calc.java
@@ -0,0 +1,18 @@
+// 1 - Pacote
+package devcalc;
+// 2 - Bibliotecas
+
+// 3 - Classe
+public class Calc {
+    // 3.1 - Atributos
+
+    // 3.2 - Métodos e Funções
+    public static void main(String[] args){
+        System.out.println("Passou pelo método main");
+        somarDoisNumeros();
+    }
+
+    public static void somarDoisNumeros(){
+        System.out.println("Passou pelo método somarDoisNumeros");
+    }
+}
